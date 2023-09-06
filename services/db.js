1ce0b390c84c5714aac4665e11c9da65b0215890
AkashMohan322 <mohanakash322@gmail.com>
2023-09-06 22:57:31 +0530

    BankApp-backend

---
+++ services/db.js
@@ -0,0 +1,22 @@
+//Mongodb connection with Nodejs
+
+//1. Connection Library - mongoose - npm i mongoose
+
+//import mongoose
+const mongoose = require('mongoose')
+
+//2. Define connection between mongoose and node
+mongoose.connect('mongodb://localhost:27017/Bankapp')
+
+//3. Create a model and schema for storing data
+const User=mongoose.model('User',{
+    username:String,
+    acno:Number,
+    password:String,
+    balance:Number,
+    transactions:[]
+})
+
+module.exports={
+    User
+}
